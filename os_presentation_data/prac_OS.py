c60b08567f66dcd3559e1d2f266b122a02482cf1
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-11-10 21:38:40 +0200

    #

--- os_presentation_data/prac_OS.py
+++ os_presentation_data/prac_OS.py
@@ -1,22 +1,10 @@
 from inspect import getmembers , isfunction ,isclass , ismethod
 import os ,signal
 
-pid = 14044
-# os.kill(pid, signal.SIGTERM)##terminate
+cwd = os.getcwd()
+print(cwd)
 
-# print ( getmembers(os,isfunction))
-# os.system("cls")
-# print(type(os.name))
-# print(os.error)
-# os.rmdir("testforshell")
-# print(os.getcwd())
-# cwd = os.getcwd()
-# os.chdir("../")
-# print ( cwd )
-# for dirpath,dirnames,filenames in  os.walk(cwd) :
-#     print (dirpath)
-#     print ( dirnames) 
-#     print (filenames)    
+os.system("tree")
 
-os.popen("draft+sources.txt")
+
 
