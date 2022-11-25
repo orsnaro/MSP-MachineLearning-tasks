6d126fed14d99040e1b0fe4a209be0a987ffb4b6
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-11-25 02:52:09 +0200

    #

---
+++ main.py
@@ -0,0 +1,5 @@
+from sub import *
+
+if __name__ == "__main__":
+    while ( super.run ) :  #program main menue
+        pass
---
+++ super.py
@@ -0,0 +1,40 @@
+import math
+import numpy as np
+import cv2
+import datetime
+import random
+from enum import Enum
+# import os
+
+# global vars and methods
+run: bool = True
+
+
+def main_menu():
+    pass
+
+
+class error_enums(Enum):
+    pass
+
+
+
+    # super classes
+class Person:
+    pass
+
+
+class Building:
+    pass
+
+
+class Speciality:
+    pass
+
+
+class Log:
+    pass
+
+
+class Cardentials:  # TODO : use has function and dictionary
+    pass
