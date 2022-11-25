1aa16e33021e61c565d015a20704e8b888ee64d2
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-11-25 02:52:09 +0200

    #

---
+++ sub.py
@@ -0,0 +1,20 @@
+from super import *
+
+#subs from person
+class prof ( Person ) :
+    pass
+
+class employe ( Person ) :
+    pass
+
+class student ( Person ) :
+    pass
+
+#building subs 
+
+
+
+
+
+#sepciality subs
+
