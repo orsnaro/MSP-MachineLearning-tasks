71276185452b3072e3fbb37b6ef4b00483304f48
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-11-26 02:21:41 +0200

    # #

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from sub import *
 from spr import *
 
-
 if __name__ == "__main__":
-    print(" \n \n --------------------- Welcome to Omar's Engineering College System ----------------------\n\n")
+    print(" \n \n ---------------------( Welcome to Omar's Engineering College System )----------------------\n\n")
     while  run :
         if main_menu() == enm.MAIN_MEN_QUIT :
             sys.exit(0)
