bc82ab60bdc31e7934ccdaae5ba087a1183e0cbe
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2022-11-28 05:46:29 +0200

    # done  childs of person + get_extra_data method + fixed some minor crnt user bugs (not tested)#

--- assignment3(college_system)/sub.py
+++ assignment3(college_system)/sub.py
@@ -2,15 +2,16 @@ from spr import *
 
    #----------------------------------------CHILD CLASSES -----------------------------------------#
 #prof rank: teach asis , lecturer , asis.prof ,  associate.prof , prof 
-#salary :  i we all want to know :) #speciality #courses he teach 
+#salary :  i know we all want to know :) #speciality #courses he teach 
 class Prof ( Person ) : 
    def __init__(self, id: str, name: str, age: int, is_prof: bool, speciality: str , rank : int , salary : float) -> None:
       super().__init__(id, name, age, is_prof, speciality)
       self.rank = rank
       self.salary = salary
+      self.save_id()
 
-      def save_id(self , id) : 
-         Person.ids_prof.append(id)
+      def save_id(self) : 
+         Person.ids_prof.append([self.id] = [f"name : {self.name}" , f"age : {self.age}" , f"Previlage : {'Professor' if self.is_prof == True else 'Student' }" , f"Department : {self.speciality}" , f"Academic Rank : {self.rank}" , f"Salary : {self.salary}"  ] )
 
 
 class Student ( Person ) :  #study_year  #current_GPA  #fees_must_pay
@@ -19,18 +20,18 @@ class Student ( Person ) :  #study_year  #current_GPA  #fees_must_pay
       self.year = year
       self.GPA = GPA
 
-      def save_id(self , id) : 
-         Person.ids_student.append(id)
+      def save_id(self) : 
+          Person.ids_student.append([self.id] = [f"name : {self.name}" , f"age : {self.age}" , f"Previlage : {'Professor' if self.is_prof == True else 'Student' }" , f"Department : {self.speciality}" , f"Study Year : {self.year}" , f"Current GPA : {self.GPA}"  ] ) 
 
 
 
 
-# class Lab (Building) : ...  #main_tools  #speciality 
-# class hall (Building) : ...  #shedule of profs name and time they come  #hell : type events hall or course lecture hall?
+class Lab (Building) : ...  #TODO #main_tools  #speciality 
+class hall (Building) : ...  #TODO #shedule of profs name and time they come  #hell : type events hall or course lecture hall?
     
 
 # CHILDS OF SPECIALITY ( TODO soon )
-# class Mech (Speciality) : ... #mech course list
-# class Elec (Speciality) : ...#Elec course list
+class Mech (Speciality) : ... #mech course list
+class Elec (Speciality) : ...#Elec course list
 
 
