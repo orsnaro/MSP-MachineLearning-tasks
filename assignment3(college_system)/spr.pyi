bb1df1b6e90c99a17a1e204788c561ae8618f3bb
omar rashad salem <77698890+orsnaro@users.noreply.github.com>
2022-11-29 10:00:55 +0200

    # commenting and adding .pyi files to view main classes and methods protoype  easier#

--- assignment3(college_system)/spr.pyi
+++ assignment3(college_system)/spr.pyi
@@ -1,3 +1,4 @@
+## this is more clear and abstract view on spr.pyi file methods and classes##
 ##########################################################################################
 #                                                                                        #
 #                                                                                        #
@@ -5,35 +6,10 @@
 #                          Programmer : Omar Rashad                                      #
 #                          Version : v0.4 Beta                                           #
 #                          Date :  27 / 11 / 2022                                        #
-#                          Code Type : enm.py.main.Mini_proj for MSP-ML Committe         #
+#                          Code Type : spr.pyi.main.Mini_proj for MSP-ML Committe        #
 #                          Title : Omar's Emgineering College System                     #
 #                          Interpreter : cPython  v3.11.0 [Compiler : MSC v.1933 AMD64]  #
 #                                                                                        #
 #                                                                                        #
 #                                                                                        #
 ##########################################################################################
-
-class enm: #could be done using dict 
-    """since python doesnt has enum data-type 
-    all funtion states will use this values 
-    to implement good program signaling"""
-    MAIN_MEN_OK = 0
-    MAIN_MEN_QUIT = 1
-    MAIN_MEN_ER = 2
-    MAIN_MEN_RES = 20
-    CON_ER = 3
-    CON_BAD_DATA = 4
-    CON_OK = 5
-    CON_NEW = 6
-    CON_LOG = 7
-    GMAIL_OK = 8
-    GMAIL_BAD = 9
-    CRED_OK = 10
-    CRED_FAIL = 11
-    CRED_ID_FOUND = 12
-    CRED_ID_NEW = 13
-    CRED_DONE = 14
-    ID_PROF = 15
-    ID_STU = 16
-    UNKNOWN = 100
-    
--- assignment3(college_system)/sub.py
+++ assignment3(college_system)/sub.py
@@ -1,3 +1,18 @@
+##########################################################################################
+#                                                                                        #
+#                                                                                        #
+#                                                                                        #
+#                          Programmer : Omar Rashad                                      #
+#                          Version : v0.4 Beta                                           #
+#                          Date :  27 / 11 / 2022                                        #
+#                          Code Type : sub.py.Mini_proj for MSP-ML Committe              #
+#                          Title : Omar's Emgineering College System                     #
+#                          Interpreter : cPython  v3.11.0 [Compiler : MSC v.1933 AMD64]  #
+#                                                                                        #
+#                                                                                        #
+#                                                                                        #
+##########################################################################################
+
 from spr import *
 
    #----------------------------------------CHILD CLASSES -----------------------------------------#
